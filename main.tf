51670fe6a7fbbb812e74bf53b64f93c0664bed13
hopetonmon <hopetonmon@gmail.com>
2025-04-23 17:59:02 +0300

    Some git inores, the provider established, variables created, and VPC started

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 4.0"
+        }
+    }
+    
+    required_version = ">= 1.0.0"
+}
+
+#-----------------------VARIABLES-----------------------------
+variable "TERRY_ACCESS_KEY" {
+    description = "Terry's AWS Access Key"
+    type        = string
+    sensitive = true
+}
+
+variable "TERRY_SECRET_KEY" {
+    description = "Terry's AWS Secret Key"
+    type        = string
+    sensitive = true
+  
+}
+
+variable "AWS_REGION" {
+    description = "AWS Region"
+    type        = string
+}
+
+#------------------PROVIDERS----------------------
+provider "aws" {
+    region     = var.AWS_REGION
+    access_key = var.TERRY_ACCESS_KEY
+    secret_key = var.TERRY_SECRET_KEY
+  
+}
+
+#-------------------VPC---------------------
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0/16"
