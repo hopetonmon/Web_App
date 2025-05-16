53d3bc07eb990bd6a833a886d0be51a9febd2873
hopetonmon <hopetonmon@gmail.com>
2025-05-16 07:31:01 +0300

    Created Listener for LB

--- main.tf
+++ main.tf
@@ -156,6 +156,22 @@ resource "aws_lb_target_group" "web_target_group" { #Target Group is used by the
         Name = "web_target_group"
     }
 }
+
+#-------------------LISTENER---------------------
+resource "aws_lb_listener" "web_listener" {
+    load_balancer_arn = aws_lb.web_alb.arn
+    port              = 80
+    protocol          = "HTTP"
+
+    default_action {
+        type = "forward"
+        target_group_arn = aws_lb_target_group.web_target_group.arn
+    }
+
+    tags = {
+        Name = "web_listener"
+    }
+}
 #-------------------SECURITY GROUP---------------------
 resource "aws_security_group" "web_sg" {
     vpc_id = aws_vpc.web_vpc.id
