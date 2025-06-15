ccf90c59201c1901037488e56f7e0c94c535964e
hopeton mon <hopetonmon@gmail.com>
2025-06-15 06:09:01 +0000

    Added ingress traffic on port 443 to alb_sg

--- main.tf
+++ main.tf
@@ -321,6 +321,13 @@ resource "aws_security_group" "alb_sg" { #Security Group for ALB
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+    ingress {
+    description = "HTTPS from anywhere"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
