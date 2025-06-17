f089aa71e47752ec65a633740e932ee9b9a1b2c6
hopetonmon <hopetonmon@gmail.com>
2025-06-17 19:57:14 +0300

    Note update

--- main.tf
+++ main.tf
@@ -286,13 +286,13 @@ resource "aws_security_group" "web_sg" {
         from_port   = 80
         to_port     = 80
         protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"] # Allow HTTP traffic from anywhere
+        cidr_blocks = ["0.0.0.0/0"] #Allow HTTP traffic from anywhere
     } 
     ingress {
         from_port   = 22
         to_port     = 22
         protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"] # Allow SSH traffic from any(where
+        cidr_blocks = ["0.0.0.0/0"] #Allow SSH traffic from any(where
     }
     ingress {
         from_port   = 443
@@ -303,7 +303,7 @@ resource "aws_security_group" "web_sg" {
     egress {
         from_port   = 0
         to_port     = 0
-        protocol    = "-1" # Allow all outbound traffic with any protocol
+        protocol    = "-1" #Allow all outbound traffic with any protocol
         cidr_blocks = ["0.0.0.0/0"]
     }
     tags = {
